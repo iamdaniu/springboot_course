0b368a6b857fffd72515f4fe2e6ba319f383005d
unknown <joernspam@gmx.de>
2018-05-05 08:38:28 +0200

    initial; fixed min and max properties

---
+++ src/main/java/com/in28minutes/microservices/limitsservice/bean/LimitConfiguration.java
@@ -0,0 +1,36 @@
+package com.in28minutes.microservices.limitsservice.bean;
+
+public class LimitConfiguration {
+	int minimum;
+	int maximum;
+
+	public LimitConfiguration() {
+	}
+
+	public LimitConfiguration(int minimum, int maximum) {
+		super();
+		this.minimum = minimum;
+		this.maximum = maximum;
+	}
+
+	public int getMinimum() {
+		return minimum;
+	}
+
+	public void setMinimum(int minimum) {
+		this.minimum = minimum;
+	}
+
+	public int getMaximum() {
+		return maximum;
+	}
+
+	public void setMaximum(int maximum) {
+		this.maximum = maximum;
+	}
+
+	@Override
+	public String toString() {
+		return "LimitConfiguration [minimum=" + minimum + ", maximum=" + maximum + "]";
+	}
+}

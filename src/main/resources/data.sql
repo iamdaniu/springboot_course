f6f6e774f11b5a1e4cb838154630d8deec577531
Jörn <joernspam@gmx.de>
2018-05-10 09:36:26 +0200

    DB for exchange rates

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+insert into exchange_value(id,currency_from,currency_to,conversion_multiple,port)
+values(1000L, 'USD', 'EUR', 1.1, 0);
+insert into exchange_value(id,currency_from,currency_to,conversion_multiple,port)
+values(1001L, 'USD', 'INR', 65, 0);
+insert into exchange_value(id,currency_from,currency_to,conversion_multiple,port)
+values(1002L, 'USD', 'AUD', 1.2, 0);

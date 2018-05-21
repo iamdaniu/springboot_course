30147048cec8a7c7c7fb6bd16cc33450d9aacdb4
Jörn <joernspam@gmx.de>
2018-05-21 08:36:34 +0200

    Filter added

---
+++ src/main/java/com/in28minutes/microservices/netflixzuulapigatewayserver/ZuulLoggingFilter.java
@@ -0,0 +1,40 @@
+package com.in28minutes.microservices.netflixzuulapigatewayserver;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+
+@Component
+public class ZuulLoggingFilter extends ZuulFilter {
+	private static final Logger LOGGER = LoggerFactory.getLogger(ZuulLoggingFilter.class);
+
+	@Override
+	public boolean shouldFilter() {
+		return true;
+	}
+
+	@Override
+	public Object run() throws ZuulException {
+		HttpServletRequest request = RequestContext.getCurrentContext().getRequest();
+		LOGGER.info("request -> {} : request URI -> {}", request, request.getRequestURI());
+		return null;
+	}
+
+	@Override
+	public String filterType() {
+		// pre, route, post, error
+		return "pre";
+	}
+
+	@Override
+	public int filterOrder() {
+		return 1;
+	}
+
+}

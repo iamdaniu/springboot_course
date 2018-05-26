f6bb4d4360ea0e79b4dd26932aba41c3691b1035
Jörn <joernspam@gmx.de>
2018-05-26 12:16:36 +0200

    Adding Hystrix resilience

--- src/main/java/com/in28minutes/microservices/limitsservice/LimitsConfigurationController.java
+++ src/main/java/com/in28minutes/microservices/limitsservice/LimitsConfigurationController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.in28minutes.microservices.limitsservice.bean.LimitConfiguration;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 @RestController
 public class LimitsConfigurationController {
@@ -15,5 +16,15 @@ public class LimitsConfigurationController {
 	@GetMapping("/limits")
 	public LimitConfiguration retrieveLimitsFromConfigurations() {
 		return new LimitConfiguration(configuration.getMinimum(), configuration.getMaximum());
+	}
+
+	@GetMapping("/fault-tolerance-example")
+	@HystrixCommand(fallbackMethod = "fallback")
+	public LimitConfiguration retrieveConfig() {
+		throw new RuntimeException("failing on purpose");
+	}
+
+	LimitConfiguration fallback() {
+		return new LimitConfiguration(4, 4444);
 	}
 }
